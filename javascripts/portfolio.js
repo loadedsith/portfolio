ad83b2d8fd10985490f06269f39af4bd8fcc2f35
Graham P Heath <graham.p.heath@gmail.com>
2013-06-24 22:33:59 -0500

    added a backbone router to handle urls, sweet! (Forgot to commit a few files)

--- javascripts/portfolio.js
+++ javascripts/portfolio.js
@@ -1,36 +1,54 @@
 $(document).ready(function(){
   $(document).foundation();
+  Backbone.history.start();
   
   $('.moreInfo').on('click', function(e) {
-    
-    e.preventDefault();
-    $('ul').each(function(){
-      if($(this).hasClass('open')){
-       // $(this).close();
-//          $(this).removeClass('open')
-        $(this).hide();
-        console.log($(this));
-        console.log("is ready");
-      }
+       e.preventDefault();
+       document.location.href = "./#"+this.getAttribute('href');
+        try{
+          $('ul').each(function(){
+            if($(this).hasClass('open')){
+             // $(this).close();
+             // $(this).removeClass('open')
+              $(this).hide();
+                }
+              });
+            }catch(e) {}
+       });
 
-    });
-    
-    $.ajax({
-      url: this.getAttribute('href'),
-      dataType: 'html',
-      method: 'get',
-      success: function(data, status, xhr) {
+   $("#jsboxes").css("height","0px");
+   $("#jsboxes").css("overflow","visible");
 
-  	      var new_service = $(data).filter(".wrapper").html();
-  			  console.log(new_service);
-  	      $('#moreInfo').empty().append("<a class='close-reveal-modal' href='#'> × </a>"+new_service);
-           $('#moreInfo').foundation('reveal', 'open');
+   });
+
+    var PortfolioRouter = Backbone.Router.extend({
+      routes:{
+        "":"doNothing",
+        "*page":"loadAjaxPage",
+      },
+      doNothing:function(){
+     //   alert('doNothing');
+        
+      },
+      loadAjaxPage:function(page){
+//        alert('loadAjaxPage: ' + page);
+        $.ajax({
+            url: page,
+            dataType: 'html',
+            method: 'get',
+            success: function(data, status, xhr) {
+
+                var new_service = $(data).filter(".wrapper").html();
+                console.log(new_service);
+                $('#moreInfo').empty().append("<a class='close-reveal-modal' href='#'> × </a>"+new_service);
+                 $('#moreInfo').foundation('reveal', 'open');
            
-      }
+            }
+         });
+       }
+      
     });
-  });
+    var portfolioRouter = new PortfolioRouter();
 
-$("#jsboxes").css("height","0px");
-$("#jsboxes").css("overflow","visible");
+  
 
-});
