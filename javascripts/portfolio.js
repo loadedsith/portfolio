be37b1919a101140c9f96f5c5a93e085e7833b4e
Graham P Heath <graham.p.heath@gmail.com>
2013-07-06 23:30:03 -0500

    not really sure, uncommitted changes… :-\

--- javascripts/portfolio.js
+++ javascripts/portfolio.js
@@ -1,54 +1,50 @@
+var PortfolioRouter = Backbone.Router.extend({
+  
+  routes:{
+    "":"doNothing",
+    ":page":"loadAjaxPage",
+  },
+  doNothing:function(){
+    // alert('doNothing');
+    
+  },
+  loadAjaxPage:function(page){
+    // alert("fragment:" +Backbone.history.fragment);
+    $.ajax({
+        url: page,
+        dataType: 'html',
+        method: 'get',
+        success: function(data, status, xhr) {
+          var pageData = $(data).filter(".wrapper").html();
+          $('#moreInfoContent').html(pageData);
+          $('#moreInfo').foundation('reveal', 'open');
+        },
+     });
+  }
+});
+
+var portfolioRouter = new PortfolioRouter();
+
 $(document).ready(function(){
   $(document).foundation();
   Backbone.history.start();
-  
+
   $('.moreInfo').on('click', function(e) {
-       e.preventDefault();
-       document.location.href = "./#"+this.getAttribute('href');
-        try{
-          $('ul').each(function(){
-            if($(this).hasClass('open')){
-             // $(this).close();
-             // $(this).removeClass('open')
-              $(this).hide();
-                }
-              });
-            }catch(e) {}
-       });
+    e.preventDefault();
+    portfolioRouter.navigate(this.getAttribute('href'),{trigger: true});
+    // 
+    try{
+      $('ul.content').each(function(){
+      if($(this).hasClass('open')){
+        $(this)
+          .css(Foundation.rtl ? 'right':'left', '-99999px')
+          .removeClass('open');
+        $(this).trigger('closed');
+        }
+      });
+    }catch(e) {}
+  });
+  $("#jsboxes").css("height","0px");
+  $("#jsboxes").css("overflow","visible");
 
-   $("#jsboxes").css("height","0px");
-   $("#jsboxes").css("overflow","visible");
-
-   });
-
-    var PortfolioRouter = Backbone.Router.extend({
-      routes:{
-        "":"doNothing",
-        "*page":"loadAjaxPage",
-      },
-      doNothing:function(){
-     //   alert('doNothing');
-        
-      },
-      loadAjaxPage:function(page){
-//        alert('loadAjaxPage: ' + page);
-        $.ajax({
-            url: page,
-            dataType: 'html',
-            method: 'get',
-            success: function(data, status, xhr) {
-
-                var new_service = $(data).filter(".wrapper").html();
-                console.log(new_service);
-                $('#moreInfo').empty().append("<a class='close-reveal-modal' href='#'> × </a>"+new_service);
-                 $('#moreInfo').foundation('reveal', 'open');
-           
-            }
-         });
-       }
-      
-    });
-    var portfolioRouter = new PortfolioRouter();
-
-  
-
+ });
